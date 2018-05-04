32400353a0821d04a048d604ca7f1a6753c7d946
fabian <fabian.zamudio.89@gmail.com>
2018-05-04 10:55:03 -0300

    Funcionalidad para almacenar los chuncks en binario

--- indexador.py
+++ indexador.py
@@ -5,23 +5,31 @@ import sys
 import codecs
 import resource
 import os
-from os import makedirs
 from os.path import join, isdir
+from os import walk, makedirs
 from struct import Struct
 import subprocess
 
 class Tokenizador(object):
-    FORMATO_POSTING = "I" # DOC_ID
+    FORMATO_POSTING = "I I" # DOC_ID | TF
     FORMATO_INDICE = "I I I" # ID_TERM DF PUNTERO_POSTING
 
     def __init__(self, path_corpus):
         self.path_corpus = path_corpus
         self.initStructures()
         self.lexicon = {} # Termino | DF
-        self.documentos = os.listdir(path_corpus)
+        self.terminos = []
+        self.documentos = self.getDocs(path_corpus)
         self.nombre_doc_actual = None
         self.doc_actual_tiene_terminos = None
         self.crear_directorio()
+
+    def getDocs(self, path_corpus):
+        nombre_doc = []
+        for raiz, dirs, nombres_docs in walk(unicode(self.path_corpus)):
+            # Protección para no leer archivos de sistema MAC ej: .DS_store
+            nombres_docs = [item for item in nombres_docs if item[0] != u'.']
+        return nombres_docs
 
     def initStructures(self):
         self.triples = []
@@ -32,37 +40,72 @@ class Tokenizador(object):
         while index_doc < len(self.documentos):
             curDoc = self.documentos[index_doc]
             try:
-                triplesDoc = self.processDoc(curDoc)
-                self.triples.append(triplesDoc)
+                # Agregamos a la tripla general las del documento actual
+                self.triples.append(self.generateTriplesFromTokens(self.processDoc(curDoc),index_doc))
                 index_doc += 1
             except MemoryError as error:
                 print "MEMORY ERROR!"
                 # guardar chunk y limpiar estructuras!
-                self.guardarChunk()
+                self.guardarChunkParcial()
                 self.initStructures()
+        # Guardamos el resto de chunks que no se guardaron
+        self.guardarChunkParcial()
+        self.sortChunk()
+        self.saveChunksToBinarios()
+        self.guardar_documentos()
+        self.guardar_terminos()
+
+    def sortChunk(self):
+        subprocess.call("sort index/chunks.txt -o index/chunks.txt", shell=True)
+
+    def saveChunksToBinarios(self):
+        packer = Struct(self.FORMATO_POSTING)
+        packerLexicon = Struct(self.FORMATO_INDICE)
+        puntero = 0
+        with open('index/lexicon.bin', mode="wb") as file_lexicon:
+            with open('index/postings.bin', mode="wb") as file_postings:
+                with open('index/chunks.txt', mode="r") as file_chunks:
+                    for tripla in file_chunks:
+                        actual_term = None
+                        tripla = tripla.split(',')
+                        if(actual_term != tripla[0]):
+                            actual_term = tripla[0]
+                            file_lexicon.write(packerLexicon.pack(self.terminos.index(actual_term), self.lexicon[actual_term], puntero))
+                        # Guardamos el DOC_ID | TF
+                        file_postings.write(packer.pack(int(tripla[1]), int(tripla[2])))
+                        puntero += packer.size
 
     def processDoc(self, doc_name):
-        with codecs.open(self.path_corpus+'/'+doc_name, mode="w", encoding="utf-8") as file_chunks:
+        with codecs.open(self.path_corpus+'/'+doc_name, mode="r", encoding="utf-8", errors='ignore') as file_doc:
+            texto_doc = file_doc.read()
+            # Tokenizamos el texto (lo más simple posible para llenar la memoria)
+            return Tokenizador.tokenizar(texto_doc)
+
+    def generateTriplesFromTokens(self, tokens, doc_id):
+        triplesDoc = {}
+        triplesDoc[doc_id] = {}
+        for token in tokens:
+            # Si no lo tengo en los terminos lo agrego
+            if token not in self.terminos:
+                self.terminos.append(token)
+            # Aumento el DF del termino
+            if token not in self.lexicon:
+                self.lexicon[token] = 0
+            self.lexicon[token] += 1
+            # Genero el dic para ir sumando los tf
+            if token not in triplesDoc[doc_id]:
+                triplesDoc[doc_id][token] = 0
+            triplesDoc[doc_id][token] += 1
+        return triplesDoc
             
 
-    def guardarChunk(self):
+    def guardarChunkParcial(self):
         with codecs.open('index/chunks.txt', mode="w", encoding="utf-8") as file_chunks:
-            for triple in self.triples:
-                file_chunks.write(triple[0]+','+triple[1]+','+triple[2] + "\n")
-
-    def update_vocabulario(self, tokens, doc_id):
-        # Recorremos los tokens
-        for token in tokens:
-            # Si el termino no esta en el vocabulario lo agrego
-            if token not in self.lexicon:
-                self.lexicon[token] = 0 # Document frecuency
-                self.posting[token] = {} # array de doc_id
-            # Si el doc_id no esta en el listado de docs de ese token lo agrego
-            if doc_id not in self.posting[token]:
-                self.posting[token][doc_id] = 1 # Agrego el doc_id a la lista de posting
-                self.lexicon[token] += 1 # Aumento en 1 el documento frecuency
-            else:
-                self.posting[token][doc_id] += 1 # Aumento el TF del doc_id
+            for tripleDoc in self.triples:
+                # Por cada triple list del doc
+                for doc_id in tripleDoc:
+                    for token in tripleDoc[doc_id]:
+                        file_chunks.write(token +','+ str(doc_id) +','+ str(tripleDoc[doc_id][token]) + "\n")
 
     @staticmethod
     def translate(to_translate):
@@ -71,14 +114,6 @@ class Tokenizador(object):
         tabin = [ord(char) for char in tabin]
         translate_table = dict(zip(tabin, tabout))
         return to_translate.translate(translate_table)
-
-    @staticmethod
-    def minMaxLargo(tokens):
-        tokensAux = []
-        for token in tokens:
-            if len(token) >= 3 and len(token) <= 20 :
-                tokensAux.append(token)
-        return tokensAux
 
     # Tokenizador básico
     @staticmethod
@@ -100,33 +135,15 @@ class Tokenizador(object):
             if not isdir('index'):
                 raise
 
-    def guardar_terminos(self):
-        with codecs.open('index/terminos.txt', mode="w", encoding="utf-8") as file_terminos:
-            for termino in self.terminos:
-                file_terminos.write(termino + "\n")
-
     def guardar_documentos(self):
         with codecs.open('index/documentos.txt', mode="w", encoding="utf-8") as file_documentos:
             for nombre_doc in self.documentos:
                 file_documentos.write(nombre_doc + "\n")
 
-    def guardar_postings(self):
-        packer = Struct(self.FORMATO_POSTING)
-        puntero_posting = 0
-        with open('index/postings.bin', mode="wb") as file_postings:
-            for id_termino, termino in enumerate(self.terminos):
-                # Por cada documento guardo el doc_id
-                for doc_id in sorted(self.posting[termino]):
-                    file_postings.write(packer.pack(doc_id))
-
-    def guardar_indice(self):
-        packer = Struct(self.FORMATO_INDICE)
-        puntero_posting = 0
-        with open('index/lexicon.bin', mode="wb") as file_lexicon:
-            for id_termino, termino in enumerate(self.terminos):
-                # ID | DF | PUNTERO
-                file_lexicon.write(packer.pack(id_termino, self.lexicon[termino] ,puntero_posting)) 
-                puntero_posting += self.lexicon[termino] * 4 # DF * 4 = X Bytes
+    def guardar_terminos(self):
+        with codecs.open('index/terminos.txt', mode="w", encoding="utf-8") as file_terminos:
+            for termino in self.terminos:
+                file_terminos.write(termino + "\n")
 
 def limit_memory(maxsize):
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
